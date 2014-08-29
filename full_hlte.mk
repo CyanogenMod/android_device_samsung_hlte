29ef4c041f7516d4c233a3d2e82071affd5d7412
Ethan Chen <intervigil@gmail.com>
2014-08-28 22:27:21 -0700

    hlte: Clean up copyright info, naming
    Change-Id: If550b884d2f4ed2f4cf8bc7e9ad3e09c46744f95

--- full_hlte.mk
+++ full_hlte.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2011 The Android Open Source Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,18 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#
-# This file is the build configuration for a full Android
-# build for maguro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and maguro, hence its name.
-#
-
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from hlte device
 $(call inherit-product, device/samsung/hlte/device.mk)
 
@@ -33,4 +24,3 @@ PRODUCT_DEVICE := hltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_MODEL := hlte
-
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,1 @@
 add_lunch_combo cm_hlte-eng
-
