a00533bab5c3d600ed83f5233017e6e933afbad2
Ethan Chen <intervigil@gmail.com>
2014-08-28 22:25:09 -0700

    hlte: Set system properties for GSM HLTE devices only
    Change-Id: Id99670ee2cff06874fcb6ffb88a63d44b08f73bf

--- init/init_hlte.c
+++ init/init_hlte.c
@@ -61,13 +61,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "hltecan-user 4.4.2 KOT49H N900W8VLUCND5 release-keys");
         property_set("ro.product.model", "SM-N900W8");
         property_set("ro.product.device", "hltecan");
-    } else if (strstr(bootloader, "N900P")) {
-        /* hltespr */
-        cdma_properties("1");
-        property_set("ro.build.fingerprint", "samsung/hltespr/hltespr:4.4.2/KOT49H/N900PVPUCNAB:user/release-keys");
-        property_set("ro.build.description", "hltespr-user 4.4.2 KOT49H N900PVPUCNAB release-keys");
-        property_set("ro.product.model", "SM-N900P");
-        property_set("ro.product.device", "hltespr");
     } else if (strstr(bootloader, "N900T")) {
         /* hltetmo */
         gsm_properties();
@@ -75,24 +68,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
         property_set("ro.build.description", "hltetmo-user 4.4.2 KOT49H N900TUVUCNB4 release-keys");
         property_set("ro.product.model", "SM-N900T");
         property_set("ro.product.device", "hltetmo");
-    } else if (strstr(bootloader, "N900V")) {
-        /* hltevzw */
-        cdma_properties("0");
-        property_set("ro.build.fingerprint", "samsung/hltevzw/hltevzw:4.4.2/KOT49H/N900VVRUCNC2:user/release-keys");
-        property_set("ro.build.description", "hltevzw-user 4.4.2 KOT49H N900VVRUCNC2 release-keys");
-        property_set("ro.product.model", "SM-N900V");
-        property_set("ro.product.device", "hltevzw");
-        property_set("ro.cdma.home.operator.alpha", "Verizon");
-        property_set("ro.cdma.home.operator.numeric", "311480");
-    } else if (strstr(bootloader, "N900R4")) {
-        /* hlteusc */
-        cdma_properties("0");
-        property_set("ro.build.fingerprint", "samsung/hlteusc/hlteusc:4.4.2/KOT49H/N900R4TYUCNAB:user/release-keys");
-        property_set("ro.build.description", "hlteusc-user 4.4.2 KOT49H N900R4TYUCNAB release-keys");
-        property_set("ro.product.model", "SM-N900R4");
-        property_set("ro.product.device", "hlteusc");
-        property_set("ro.cdma.home.operator.alpha", "U.S.Cellular");
-        property_set("ro.cdma.home.operator.numeric", "311580");
     } else {
         /* hltexx */
         gsm_properties();
@@ -111,12 +86,4 @@ void gsm_properties()
     property_set("ro.telephony.default_network", "9");
     property_set("ro.telephony.ril.v3", "newDialCode");
     property_set("telephony.lteOnGsmDevice", "1");
-}
-
-void cdma_properties(char cdma_sub[])
-{
-    property_set("ro.telephony.default_cdma_sub", cdma_sub); // 0: RUIM/SIM  1: NV
-    property_set("ro.telephony.default_network", "10");
-    property_set("ro.telephony.ril.v3", "newDriverCallU,newDialCode");
-    property_set("telephony.lteOnCdmaDevice", "1");
 }
